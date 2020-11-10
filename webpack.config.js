bc810315ad131ede31c976052c0ddfcca9be1b67
klr <1453931154@qq.com>
2020-11-10 11:55:13 +0800

    00

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,9 @@
-const {resolve} = require('path');
+const path = require('path');
+const uglify = require('uglifyjs-webpack-plugin');
+const htmlWebpackPlugin = require('html-webpack-plugin');
 const autoprefixer = require('autoprefixer');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+const miniCssExtractPlugin = require('mini-css-extract-plugin')
+
 module.exports = {
     mode: 'development',
     devtool: 'source-map',
@@ -10,119 +13,120 @@ module.exports = {
     },
     //多文件入口
     entry: {
-        index: resolve(__dirname, './src/js/index.js'),
-        detail: resolve(__dirname, './src/js/detail.js'),
-        collections: resolve(__dirname, './src/js/collections.js'),
+        index: path.resolve(__dirname, './src/js/index.js'),
+        detail: path.resolve(__dirname, './src/js/detail.js'),
+        collections: path.resolve(__dirname, './src/js/collections.js'),
     },
     //输出/打包设置
-    output:{
+    output: {
         //路径
-        path:resolve(__dirname,'./dist'),
+        path: path.resolve(__dirname, './dist'),
         //打包后的文件名
-        filename:'js/[name].js'
+        filename: 'js/[name].js'
     },
     //模块设置
-    module:{
+    module: {
         //模块的匹配规则
-        rules:[
-            {
-                test:/\.js$/,
-                loader:'babel-loader',
-                exclude:resolve(__dirname,'node_modules'),
-                query:{
-                    'presets':['latest']
+        rules: [{
+                test: /\.js$/,
+                loader: 'babel-loader',
+                exclude: path.resolve(__dirname, 'node_modules'),
+                query: {
+                    'presets': ['latest']
                 }
             },
             {
-                test:/\.tpl$/,
-                loader:'ejs-loader'
+                test: /\.tpl$/,
+                loader: 'ejs-loader'
             },
             {
-                test:/\/.css$/,
-               use:[
-                   'style-loader',
-                   'css-loader',
-                   {
-                       loader:'postcss-loader',
-                       options:{
-                           plugins:function(){
-                               return [autoprefixer('last 5 version')]
-                           }
-                       }
-                   }
-               ]
+                test: /\.css$/,
+                use: [{
+                        loader: miniCssExtractPlugin.loader,
+                        options: {
+                            //在node开发版本下进行热更新
+                            hmr: process.env.NODE_ENV === 'development'
+                        }
+                    },
+                    // 'style-loder',
+                    'css-loader',
+                    {
+                        loader: 'postcss-loader',
+                        options: {
+                            plugins: function() {
+                                return [autoprefixer('last 5 version')]
+                            }
+                        }
+                    },
+                    'sass-loader',
+                ]
             },
             {
-                test:/\/.scss$/,
-               use:[
-                   'style-loader',
-                   'css-loader',
-                   {
-                       loader:'postcss-loader',
-                       options:{
-                           plugins:function(){
-                               return [autoprefixer('last 5 version')]
-                           }
-                       }
-                   },
-                   'sass-loader'
-               ]
-            },
-            {
-                test:/\.(png|jpg|jpeg|gif|ico|woff|eot|svg|ttf)$/i,
-                loaders: 'url-loader?limit=1024&name=img/[name]-[hash:16].[ext]',
+                test: /\.(png|jpg|jpeg|gif|ico|woff|eot|svg|ttf)$/i,
+                loader: [
+                    'url-loader?limit=1024&name=img/[name]-[hash:16].[ext]',
+                    'image-webpack-loader'
+                ]
             }
         ]
     },
     //插件
-    plugins:[
-        new HtmlWebpackPlugin({
-            filename:'index.html',
-            template:resolve(__dirname,'src/index.html'),
-            title:'新闻头条',
-            chunks:['index'],
-            chunksSortMode:'manual',
-            excludeChunks:['node_modules'],
-            hash:true,
-            minify:{
-                removeComments:true,
-                collapseWhitespace:true
+    plugins: [
+        new uglify(),
+        new htmlWebpackPlugin({
+            filename: 'index.html',
+            template: path.resolve(__dirname, 'src/index.html'),
+            title: '新闻头条',
+            //多个chunk手动排序
+            chunksSortMode: 'manual',
+            chunks: ['index'],
+            excludeChunks: ['node_modules'],
+            hash: true,
+            //压缩代码
+            minify: {
+                //删除注释
+                removeComments: true,
+                //删除空格换行
+                collapseWhitespace: true
             }
         }),
-        new HtmlWebpackPlugin({
-            filename:'detail.html',
-            template:resolve(__dirname,'src/detail.html'),
-            title:'新闻详情',
-            chunks:['index'],
-            chunksSortMode:'manual',
-            excludeChunks:['node_modules'],
-            hash:true,
-            minify:{
-                removeComments:true,
-                collapseWhitespace:true
+        new miniCssExtractPlugin({
+            filename: 'css/[name].css'
+        }),
+        new htmlWebpackPlugin({
+            filename: 'detail.html',
+            template: path.resolve(__dirname, 'src/detail.html'),
+            title: '新闻详情',
+            chunks: ['index'],
+            chunksSortMode: 'manual',
+            excludeChunks: ['node_modules'],
+            hash: true,
+            minify: {
+                removeComments: true,
+                collapseWhitespace: true
             }
         }),
-        new HtmlWebpackPlugin({
-            filename:'collections.html',
-            template:resolve(__dirname,'src/collections.html'),
-            title:'我的新闻',
-            chunks:['index'],
-            chunksSortMode:'manual',
-            excludeChunks:['node_modules'],
-            hash:true,
-            minify:{
-                removeComments:true,
-                collapseWhitespace:true
+        new htmlWebpackPlugin({
+            filename: 'collections.html',
+            template: path.resolve(__dirname, 'src/collections.html'),
+            title: '我的新闻',
+            chunks: ['index'],
+            chunksSortMode: 'manual',
+            excludeChunks: ['node_modules'],
+            hash: true,
+            minify: {
+                removeComments: true,
+                collapseWhitespace: true
             }
         })
     ],
     //开发服务器的配置
-    devServer:{
-        watchOptions:{
-            ignored:/\node_modules/
+    devServer: {
+        watchOptions: {
+            ignored: /\node_modules/
         },
-        open:true,
-        host:'localhost',
-        port:3000
+        open: true,
+        host: 'localhost',
+        port: 3000
     }
 }
