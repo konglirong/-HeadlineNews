2e9d40416b3c7f218021d742c45f95735149757f
klr <1453931154@qq.com>
2020-11-17 09:05:10 +0800

    00

--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,24 @@
 import '../scss/index.scss'
-import './imports;'
+// import './imports;'
 
-console.log('index')
+// console.log('index')
+
+var city = window.prompt('请输入您所在的城市')
+
+switch (city) {
+    case '北京':
+        // console.log(20000);
+        var aaa = window.prompt('哈哈哈')
+        document.write(city + aaa)
+        break;
+    case '上海':
+        var aaa = window.prompt('哈哈哈')
+        document.write(city + aaa)
+        break
+    case '杭州':
+        var aaa = window.prompt('哈哈哈')
+        document.write(city + aaa)
+        break
+    default:
+        break;
+}
